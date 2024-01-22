29842c7de0593730f1a3638bb861395fa64b0021
ramazanix <ramazanui2002@gmail.com>
2024-01-22 13:42:14 +0500

    Created redirect page

---
+++ src/app/l/[short_name]/page.tsx
@@ -0,0 +1,12 @@
+import { urlsService } from '@/services'
+import { permanentRedirect } from 'next/navigation'
+
+export default async function RedirectPage({
+  params,
+}: {
+  params: { short_name: string }
+}) {
+  await urlsService.urls.get(params.short_name!).then((res) => {
+    permanentRedirect(res.full_name)
+  })
+}
