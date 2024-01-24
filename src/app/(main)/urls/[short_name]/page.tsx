7573151497b2f9b84bd0e2e1912b452f20d219f2
Ramazan <ramazanui2002@gmail.com>
2024-01-24 19:26:40 +0300

    Added params to useEffect deps array

--- src/app/(main)/urls/[short_name]/page.tsx
+++ src/app/(main)/urls/[short_name]/page.tsx
@@ -64,7 +64,7 @@ export default function UrlPage({
     }
 
     fetchUrlData()
-  }, [])
+  }, [params.short_name])
 
   return (
     <main className="flex select-none flex-col items-center justify-center">
